dec5eacc41f19e16914fc44f3d9cd78f6154c096
Henry Jao <henry.jao@trademe.co.nz>
2023-01-18 10:38:29 +1300

    test(delay): add a test with no delay

--- projects/my-lib/src/lib/rxjs-marbles.spec.ts
+++ projects/my-lib/src/lib/rxjs-marbles.spec.ts
@@ -1,6 +1,6 @@
 import { TestScheduler } from 'rxjs/testing';
-import { throttleTime, delay } from 'rxjs';
-import {expect, jest, test} from '@jest/globals'
+import { delay } from 'rxjs';
+import { expect, jest } from '@jest/globals';
 
 describe('Rxjs marbles', () => {
   let testScheduler: TestScheduler;
@@ -11,13 +11,23 @@ describe('Rxjs marbles', () => {
     });
   });
 
-  it('generates the stream correctly', () => {
+  it('generates the stream correctly with delay 2s', () => {
     testScheduler.run((helpers) => {
       const { time, cold, expectObservable } = helpers;
-      const source = cold('--a|');
-      const t = time('       -| ');
-      const expected = '   ---a|';
+      const source = cold('---a--b--|');
+      const t = time('        --|    '); // <- this doesn't work with Jest ESM
+      const expected = '   -----a--b|';
       const result = source.pipe(delay(t));
+      expectObservable(result).toBe(expected);
+    });
+  });
+
+  it('generates the stream correctly without delay', () => {
+    testScheduler.run((helpers) => {
+      const { cold, expectObservable } = helpers;
+      const source = cold('---a--b|');
+      const expected = '   ---a--b|';
+      const result = source;
       expectObservable(result).toBe(expected);
     });
   });
