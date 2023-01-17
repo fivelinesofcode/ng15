2b102bb4f74f4a5dd77e4629fdc57732fc3c1c08
Henry Jao <fivelinesofcode@gmail.com>
2023-01-17 23:08:47 +1300

    test: wip

--- projects/my-lib/src/lib/my-lib.service.spec.ts
+++ projects/my-lib/src/lib/my-lib.service.spec.ts
@@ -1,16 +1,35 @@
 import { TestBed } from '@angular/core/testing';
+import { TestScheduler } from 'rxjs/testing';
+import { throttleTime, delay } from 'rxjs';
+import {expect, jest, test} from '@jest/globals'
 
 import { MyLibService } from './my-lib.service';
 
 describe('MyLibService', () => {
   let service: MyLibService;
+  let testScheduler: TestScheduler;
 
   beforeEach(() => {
+    
     TestBed.configureTestingModule({});
     service = TestBed.inject(MyLibService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it.only('generates the stream correctly', () => {
+    testScheduler = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
+    });
+    testScheduler.run((helpers) => {
+      const { time, cold, expectObservable } = helpers;
+      const source = cold('--a|');
+      //const t = time('');
+      const expected = '--a|';
+      const result = source;
+      expectObservable(result).toBe(expected);
+    });
   });
 });
